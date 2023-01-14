432ea615b1a030e48067cb85045d69ea11acbf55
vivimila <violamilashevskaya@yandex.by>
2023-01-14 21:06:00 +0300

    First commit.Made the task №1

---
+++ program.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int i = random.nextInt(201);
+        System.err.println(i);
+
+    }
+  }
