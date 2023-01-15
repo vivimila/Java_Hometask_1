d1702500adc530440a6ac4496f53d1558bd23030
vivimila <violamilashevskaya@yandex.by>
2023-01-15 12:55:26 +0300

    Solved the task 2

--- programHighBit.java
+++ programHighBit.java
@@ -1,13 +1,12 @@
-import java.util.Random;
+//import java.util.Random;
 
 //2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
 
 public class programHighBit {
-    public static void main(String[] args) {
-        Random random = new Random();
-
-        int i = random.nextInt(201);
-        System.err.println(i);
+    public static int significantBit(int i) {
+        int binLen = Integer.toBinaryString(i).length();
+        return binLen;
 
     }
   }
+
