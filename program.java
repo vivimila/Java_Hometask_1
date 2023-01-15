36846eb1638fac16392de301cec5f7827e4610be
vivimila <violamilashevskaya@yandex.by>
2023-01-15 12:49:21 +0300

    Corrected the task 1

--- program.java
+++ program.java
@@ -3,6 +3,7 @@ import java.util.Random;
 /**
  * program
  */
+//1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
 public class program {
     public static void main(String[] args) {
         Random random = new Random();
