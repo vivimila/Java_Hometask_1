45148c861bc123a7f603389a87047f8d5e480af1
vivimila <violamilashevskaya@yandex.by>
2023-01-15 12:50:09 +0300

    Created the task 2

---
+++ programHighBit.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+//2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+
+public class programHighBit {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int i = random.nextInt(201);
+        System.err.println(i);
+
+    }
+  }
