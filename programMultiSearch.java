13ae30e570e745a2274495509c5c6e45cd3fc427
vivimila <violamilashevskaya@yandex.by>
2023-01-17 16:02:43 +0300

    Created the task 3,4

---
+++ programMultiSearch.java
@@ -0,0 +1,6 @@
+// 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+// 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+public class programMultiSearch {
+    
+}
